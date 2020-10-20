a4c3d22733958491f1e60dbd5b75b56233004c73
Evgeniy Mineev <e.mineev@crazypanda.ru>
2020-10-20 12:08:21 +0400

    UNITYCORE-408: add more rules for proguard support

--- Editor/Steps/AndroidMinificationOptionsStep.cs
+++ Editor/Steps/AndroidMinificationOptionsStep.cs
@@ -9,19 +9,28 @@ namespace CrazyPanda.UnityCore.BuildUtils
 {
     public sealed class AndroidMinificationOptionsStep : IRunPreBuild
     {
-        private static readonly string _pathToProguardFile = Path.Combine( Application.dataPath, "Plugins", "Android", "proguard-user.txt" );
-
-        private static readonly IEnumerable< string > _defaultProguardRules = new List< string >
+        private static readonly IEnumerable<string> _defaultProguardRules = new List<string>
         {
             "-keep class ru.crazypanda.unisocial.** { *; }",
-            "-keep class com.prime31.** { *; }"
+            "-keep class com.prime31.** { *; }",
+            "-keep class unisocial.** {*;}",
+            "-keep class com.google.android.gms.** {*;}",
+            "-keep class com.facebook.* {*;}",
+            "-keep class com.facebook.internal.* {*;}"
         };
 
         private readonly HashSet< string > _proguardRulesToAdd = new HashSet< string >();
         private readonly HashSet< string > _proguardRulesToRemove = new HashSet< string >();
 
+#if UNITY_2020_1_OR_NEWER
+        [ Option( "enableAndroidMinification" ) ]
+        public bool EnableAndroidMinification { get; private set; } = false;
+#else
         [ Option( "androidMinification" ) ]
         public AndroidMinification AndroidMinification { get; private set; } = AndroidMinification.None;
+#endif
+        [ Option( "pathToProguardFile" ) ]
+        public string _pathToProguardFile { get; private set; } = Path.Combine( Application.dataPath, "Plugins", "Android", "proguard-user.txt" );
 
         [ Option( "addProguardRule" ) ]
         public void AddProguardRule( string minificationRule )
@@ -37,15 +46,24 @@ namespace CrazyPanda.UnityCore.BuildUtils
 
         public void OnPreBuild( IStepLocator locator )
         {
+#if UNITY_2020_1_OR_NEWER
+            PlayerSettings.Android.minifyDebug = EnableAndroidMinification;
+            PlayerSettings.Android.minifyRelease = EnableAndroidMinification;
+#else
             EditorUserBuildSettings.androidDebugMinification = AndroidMinification;
             EditorUserBuildSettings.androidReleaseMinification = AndroidMinification;
+#endif
 
             WriteProguardData();
         }
 
         private void WriteProguardData()
         {
+#if UNITY_2020_1_OR_NEWER            
+            if( !EnableAndroidMinification  )
+#else
             if( AndroidMinification != AndroidMinification.Proguard )
+#endif            
             {
                 return;
             }
